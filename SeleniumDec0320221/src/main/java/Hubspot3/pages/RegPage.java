d77193121eeb3c91c13c8a2f813d1ef0cbe04ba3
Valmiki Vishwakarma <you@example.com>
2022-12-04 10:10:50 +0530

    Adding POMS.

---
+++ SeleniumDec0320221/src/main/java/Hubspot3/pages/RegPage.java
@@ -0,0 +1,21 @@
+package com.qa.hubspot.pages;
+
+class RegPage{
+
+public static void main(){
+
+  System.out.println();
+
+}
+  
+  public void m2(){
+		System.out.println("m2 method....");
+	}
+	
+	public void m3(){
+		System.out.println("m3 method....");
+	}
+
+
+
+}
---
+++ SeleniumDec0320221/src/main/java/Playwright-Java-PageObjectModel-master/src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.constants;
+
+public class AppConstants {
+	
+	
+	public static final String HOME_PAGE_TITLE = "Your Store";
+	
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
+
+
+}
